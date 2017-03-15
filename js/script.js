db7300a45326631374142148c1a026d6ba455f53
Felipe Augusto Trindade <felipextrindade@gmail.com>
2017-03-15 20:27:23 -0300

    Portfolio and new images!
    Couldn't download Git  so I needed to upload it :(

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
       $('.parallax').parallax();
+
+      $.get( "https://api.github.com/users/felipextrindade", function( data ) {
+            $("#great-title").append(data.name);
+            $("#logo").append('<img id="logo-image" src="'+ data.avatar_url +'" height="128px" width="128px">');            
+      });
 });
+
+
