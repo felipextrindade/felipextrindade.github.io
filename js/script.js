06bf3169a6f40e705db103eb022c0092e58ee08b
Felipe Augusto Trindade <felipextrindade@gmail.com>
2017-03-22 18:32:19 -0300

    Bio added

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
       $.get( "https://api.github.com/users/felipextrindade", function( data ) {
             $("#great-title").append(data.name);
-            $("#logo").append('<img id="logo-image" src="'+ data.avatar_url +'" height="128px" width="128px">');            
-      });
+            $("#logo").append('<img id="logo-image" src="'+ data.avatar_url +'" height="128px" width="128px">');
+            $('#bio').append('<span class="black-text">'+ data.bio +'</span');
 });
 
 
